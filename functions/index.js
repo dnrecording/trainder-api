2a9e505cb113d3c5847ce6bf37b9477fcf53c136
drgn <62900543+dnrecording@users.noreply.github.com>
2021-03-31 15:27:12 +0700

    init

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/users", function (req, res) {
+    let users = [
+        { name : "John Doe", age : 22},
+        { name : "Jane Smith", age : 23},
+    ]
+    res.send(users);
+  });
+
+exports.api = functions.region("asia-east2").https.onRequest(app);
