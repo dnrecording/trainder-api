c74febd246dc02ea159f95dcb88a78464d9d4881
Glairly <62010787@kmitl.ac.th>
2021-05-01 13:42:53 +0700

    Revert "ิadd Friend , end point"
    This reverts commit 01fcabca073413d33c404cd666575dfe7a1e6292.

--- functions/routes/course-routes.js
+++ functions/routes/course-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { addCourse, addEvent_toCourse, addEvent_toUser, isTableCollision, addCourseMember, joinCourse } = require('../controlllers/Course_ctrl');
+const  {addCourse , addEvent_toCourse,addEvent_toUser, isTableCollision,addCourseMember,joinCourse}  = require('../controlllers/Course_ctrl');
 
 const router = express.Router()
 
 router.get('/addCourse/:creatorid', async(req, res, next) => {
+
+    
     try {
         console.log('Adding Course')
         await addCourse(req.params.creatorid);
@@ -14,11 +16,11 @@ router.get('/addCourse/:creatorid', async(req, res, next) => {
 })
 
 router.get('/EventToCourse/:courseId/:uid', async(req, res, next) => {
-    // caution: uid base on Film 
-
+// caution: uid base on Film 
+    
     try {
         console.log('Adding Event to Course')
-        await addEvent_toCourse(req.params.courseId, req.params.uid)
+        await addEvent_toCourse(req.params.courseId,req.params.uid)
         res.send('Added successfullly')
     } catch (error) {
         res.status(400).send(error);
@@ -28,7 +30,7 @@ router.post('/EventToUser', async(req, res, next) => {
     //caution : uid base on Film
     try {
         console.log('Adding events to user table')
-        data = await addEvent_toUser(req.body.courseId, req.body.uid)
+        data = await addEvent_toUser(req.body.courseId,req.body.uid)
         res.send(data)
     } catch (error) {
         res.status(400).send(error);
@@ -36,8 +38,8 @@ router.post('/EventToUser', async(req, res, next) => {
 })
 router.get('/isCollision/:courseId/:uid', async(req, res, next) => {
     //caution : uid base on Film
-    try {
-        let ch = await isTableCollision(req.params.courseId, req.params.uid)
+    try {  
+        let ch = await isTableCollision(req.params.courseId,req.params.uid)
         console.log(ch)
         res.send('Check Collsion')
     } catch (error) {
@@ -48,7 +50,7 @@ router.get('/addMember/:courseId/:userId', async(req, res, next) => {
     //caution : uid base on Film
     try {
         console.log('Adding member')
-        await addCourseMember(req.params.courseId, req.params.userId)
+        await addCourseMember(req.params.courseId,req.params.userId)
         res.send('Added member successfullly')
     } catch (error) {
         res.status(400).send(error);
@@ -58,7 +60,7 @@ router.post('/joinCourse', async(req, res, next) => {
     //caution : uid base on Film
     try {
         console.log('Joining Course')
-        let result = await joinCourse(req.body.courseId, req.body.userId)
+        let result = await joinCourse(req.body.courseId,req.body.userId)
         res.send(result)
     } catch (error) {
         res.status(400).send(error);
@@ -70,5 +72,5 @@ router.post('/joinCourse', async(req, res, next) => {
 
 module.exports = {
     routs: router
-
+    
 }
