bbed41ed87fa6f141294a32733d5e5aa0b98fc2c
blxsheep <por2543rk@gmail.com>
2021-05-03 19:39:24 +0700

    JoinCourse -Bug Fixed

--- functions/controlllers/Course_ctrl.js
+++ functions/controlllers/Course_ctrl.js
@@ -1,6 +1,7 @@
 'use strict';
 const { admin } = require('../database');
 const { Course, myEvent } = require('../models/Course');
+const { makeid } = require("./misc");
 
 const db = admin.firestore();
 // addCoures and addEvent_toCourse just a prototype
@@ -21,47 +22,82 @@ const addCourse = async (creator_id) => {
 }
 const getEventsfromUser = async (uid) => {
     console.log('Getting all Events')
-   
+    console.log(uid)
     let tableRef = await db.collection("Table");
-    let userData = await tableRef.where("uid", "==", uid).get();
+    var userData = await tableRef.where("uid", "==", uid).get();
+    var userDocid
+    if (userData.empty) {
+        console.log('Empty')
+        var docRef = await newTable(uid)
+        console.log(docRef)
 
-    userData.forEach(doc => {
-        this.userDocid = doc.id;
-        //console.log(doc.id, '=>', doc.data());
-    });
+        userData = await tableRef.doc(docRef).get()
+        userDocid = docRef
+        var tt = await userData.data()
+        console.table(tt)
+    }
+    else {
+        console.log('Normal')
+        userData = await tableRef.where("uid", "==", uid).get();
 
-    let userEvent = await tableRef.doc(this.userDocid).collection("Event").get();
+        console.log('ddf')
+        userData.forEach(doc => {
+            console.log('Loop')
+            userDocid = doc.id;
+            //console.log(doc.id, '=>', doc.data());
+        });
+        console.log(userDocid)
+
+    }
+
+
+
+
+
+    console.log('Almost')
+    let userEvent = await tableRef.doc(userDocid).collection("Event").get();
     const Events = [];
-
-    userEvent.forEach(doc => {        
+    console.log('Good')
+    userEvent.forEach(doc => {
         if (JSON.stringify(doc.data()) != "{}") {
             let EventData = new myEvent(doc.id, doc.data().name, doc.data().creator, doc.data().start, doc.data().end, doc.data().color, doc.data().details)
-            Events.push(EventData)
+            if (EventData.start != undefined && EventData.start != null)
+                Events.push(EventData)
         }
     });
 
-
+    console.table(Events)
     return Events;
 }
-const uidToUserId = async(uid) =>{
-    const user = await db.collection("userData").where("uid","==",uid).get()
+const uidToUserId = async (uid) => {
+    const user = await db.collection("userData").where("uid", "==", uid).get()
     var userId
-    user.forEach(doc=>{
+    user.forEach(doc => {
         userId = doc.id
     })
     return userId
 }
+const newTable = async (uid) => {
+    console.log('Creating new Table')
+    const docRef = await makeid(30)
+    await db.collection("Table").doc(docRef).set({ uid: uid })
+    const event = await db.collection("Table").doc(docRef).collection("Event")
+
+    await event.doc().set({ just: "dummy" })
+    console.log('Create successful')
+    return docRef
+}
 const addEvent_toCourse = async (CourseId, uid) => {
     // copy even from userTable to Course events
     // cuation : uid base on Film
-  
+
 
     let arr = await getEventsfromUser(uid)
     let Events = [...arr]
 
 
     const course = await db.collection('Course').doc(CourseId)
-   
+
     const data = await course.get()
 
 
@@ -78,7 +114,7 @@ const addEvent_toCourse = async (CourseId, uid) => {
         list.push(...Events)
     }
 
-    
+
     const courseEvent = course.collection("Event")
     var i = 0
     for (const event of list) {
@@ -97,52 +133,54 @@ const addEvent_toCourse = async (CourseId, uid) => {
     }
 }
 const getEventsfromCourse = async (courseId) => {
-    console.log('Going in')
+    console.log('Getting Event from Course')
     const course = await db.collection('Course').doc(courseId)
     const courseEvents = await course.collection('Event').get()
-    
-    let Events  =[]
+
+    let Events = []
     courseEvents.forEach(doc => {
         let EventData = new myEvent(doc.id, doc.data().name, doc.data().creator, doc.data().start, doc.data().end, doc.data().color, doc.data().details)
-        Events.push(EventData)
+        if (EventData.start != undefined && EventData.start != null)
+            Events.push(EventData)
 
     })
+    console.log('Got it ')
 
     return Events
 
 
 
 }
-const getCourseData= async (courseId) =>{
-    
-    var data =  await (await db.collection("Course").doc(courseId).get()).data()
-    if(data ==null) {
+const getCourseData = async (courseId) => {
+
+    var data = await (await db.collection("Course").doc(courseId).get()).data()
+    if (data == null) {
         return 'No Member'
     }
     return data
 }
-const addEvent_toUser = async (courseId,uid) => {
+const addEvent_toUser = async (courseId, uid) => {
     let userId = await uidToUserId(uid)
-    
-    
-    if(await isTableCollision(courseId,userId)){
-        
+
+
+    if (await isTableCollision(courseId, userId)) {
+
         return 'Collission can not merge table '
     }
     // caution : uid base on Film
-    let Events =  await getEventsfromCourse(courseId)    
+    let Events = await getEventsfromCourse(courseId)
     const Table = await db.collection("Table")
-    const userTable_baseUID = await Table.where("uid", "==", uid).get()    
-    var table_id 
-    userTable_baseUID.forEach(doc =>{ 
+    const userTable_baseUID = await Table.where("uid", "==", uid).get()
+    var table_id
+    userTable_baseUID.forEach(doc => {
         // User always has one table       
-         table_id = doc.id
+        table_id = doc.id
     })
-    const userEvent  =await Table.doc(table_id).collection("Event")
-    
-    
+    const userEvent = await Table.doc(table_id).collection("Event")
+
+
     for (const event of Events) {
-        
+
         await userEvent.doc(event.id).set({
             id: event.id,
             name: event.name,
@@ -162,85 +200,89 @@ const addEvent_toUser = async (courseId,uid) => {
 
 
 }
-const isTableCollision  = async(courseId,userId) =>{
+const isTableCollision = async (courseId, userId) => {
     let uid = await userIdtoUID(userId)
     let CourseEvents = await getEventsfromCourse(courseId)
-    let UserEvents  = await getEventsfromUser(uid)
+    let UserEvents = await getEventsfromUser(uid)
 
-    // console.log('Course Table')
-    // console.table(CourseEvents)
-    // console.log('User table before Merge')
-    // console.table(UserEvents)
-    if(UserEvents ==[] || CourseEvents==[]){
-        
+    console.log('Course Table')
+    console.table(CourseEvents)
+    console.log('User table before Merge')
+    console.table(UserEvents)
+    console.log('Checking Collision')
+    if (UserEvents == [] || CourseEvents == []) {
+
         return false
     }
-    for( const userEvent of UserEvents){
-        for( const courseEvent of CourseEvents){
-            
-            
+    for (const userEvent of UserEvents) {
+        for (const courseEvent of CourseEvents) {
+
+
             var newEvent_start = courseEvent.start
             var newEvent_end = courseEvent.end
             var oldEvent_start = userEvent.start
-            var oldEvent_end  = userEvent.end
+            var oldEvent_end = userEvent.end
 
-          if((newEvent_start >= oldEvent_start  && newEvent_start <= oldEvent_end)
-          || (newEvent_end >=oldEvent_start && newEvent_end <= oldEvent_end)
-          || (oldEvent_start >=newEvent_start&& oldEvent_start <= newEvent_end )
-          || (oldEvent_end >= newEvent_start && oldEvent_end <= newEvent_end)){
-            console.log('%c Collide', 'background: #222 ;color: #bada55')
-            console.log(' at',courseEvent.id,' <<<<>>>>>>',userEvent.id)
-            
-            return true
-          }
+
+            if ((newEvent_start >= oldEvent_start && newEvent_start <= oldEvent_end)
+                || (newEvent_end >= oldEvent_start && newEvent_end <= oldEvent_end)
+                || (oldEvent_start >= newEvent_start && oldEvent_start <= newEvent_end)
+                || (oldEvent_end >= newEvent_start && oldEvent_end <= newEvent_end)) {
+                console.log('%c Collide', 'background: #222 ;color: #bada55')
+                console.log(' at', courseEvent.id, ' <<<<>>>>>>', userEvent.id)
+
+                return true
+            }
 
         }
-     
 
 
-        
+
+
     }
     return false
- 
+
 
 
 }
-const userIdtoUID = async(userId)=>{
+const userIdtoUID = async (userId) => {
     const user = await db.collection("userData").doc(userId)
     const data = await user.get()
     return data.data().uid
 }
-const addCourseMember  = async(courseId,userId)=>{
-    const  course = await db.collection("Course").doc(courseId)
+const addCourseMember = async (courseId, userId) => {
+    const course = await db.collection("Course").doc(courseId)
     const course_data = await course.get()
     var member = []
-    if(course_data.data().member !=null){
+    if (course_data.data().member != null) {
         member.push(...course_data.data().member)
     }
     member.push(userId)
-    await course.update({member : member})
-    
+    await course.update({ member: member })
 
-   
-    
 
-   
+
+
+
+
 
 }
-const joinCourse = async(courseId,userId)=>{
+const joinCourse = async (courseId, userId) => {
     console.log(userId)
-    const uid  = await userIdtoUID(userId)
-    
-    if(await isTableCollision(courseId,userId)){
+    const uid = await userIdtoUID(userId)
+
+    if (await isTableCollision(courseId, userId)) {
         return 'Your schedule is not ready for this course please re ararage'
     }
-    
-    await addEvent_toUser(courseId,uid)
-    await addCourseMember(courseId,userId)
+    console.log('------No collision')
+    console.log('---adding Event To user')
+    await addEvent_toUser(courseId, uid)
+    console.log('--- Adding member')
+    await addCourseMember(courseId, userId)
 
     return 'You are member of this course now '
 
 }
 module.exports = {
-    addCourse, addEvent_toCourse, addEvent_toUser,isTableCollision, addCourseMember,joinCourse,getCourseData
+    addCourse, addEvent_toCourse, addEvent_toUser, isTableCollision, addCourseMember, joinCourse, getCourseData
 }
